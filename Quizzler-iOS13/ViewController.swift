b89dfd5eddb05ecede6b885b0add7dedccef2582
Kim Ngan <kimngan.ledang@gmail.com>
2021-01-27 21:37:13 +0100

    setup simple quiz

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var progressBar: UIImageView!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    let quiz = [
+        ["4 + 2 is equal to 6", "True"],
+        ["5 - 3 is greater than 1", "True"],
+        ["3 + 8 is less than 10", "False"]
+    ]
+    var questionNumber = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
     }
 
-
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle
+        let actualAnswer = quiz[questionNumber][1]
+        
+        if userAnswer == actualAnswer{
+            print("Right!")
+        }else{
+            print("Wrong!")
+        }
+        if questionNumber + 1 < quiz.count{
+            questionNumber += 1
+        }else{
+            questionNumber = 0
+        }
+        updateUI()
+    }
+    
+    func updateUI(){
+        questionLabel.text = quiz[questionNumber][0]
+    }
 }
 
